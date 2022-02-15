ce0c3c69680732b500f44a0ecfb45fb123e6ae70
Fiona-75 <98051624+Fiona-75@users.noreply.github.com>
2022-02-15 19:32:33 +0000

    Add files via upload
    Adding Get into Tech Hoework files

---
+++ ex10_starter.py
@@ -0,0 +1,30 @@
+import sys, glob, os
+
+# Get the directory name
+if sys.platform == 'win32':
+    hdir = os.environ['HOMEPATH']
+else:
+    hdir = os.environ['HOME']
+
+# Construct a portable wildcard pattern
+pattern = os.path.join(hdir,'*')
+
+# print(os.environ)
+
+# TODO: Use the glob.glob() function to obtain the list of filenames
+
+# do i need to use a list function too? my code gives nothing. what is the actual directory?
+
+
+
+
+
+
+
+# TODO: use os.path.getsize to find each file's size
+
+# TODO: Add a test to only display files that are not zero length
+
+# TODO: Remove the leading directory name(s) from each filename before you print it - 
+# using os.path.basename()
+
